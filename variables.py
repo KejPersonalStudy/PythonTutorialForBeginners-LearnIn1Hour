215578f7eb7f90d0ba69f482c508235a5d2880a7
KejPersonalStudy <jekmena.personal.study@gmail.com>
2025-02-24 09:30:02 +1100

    Update variables.py

--- variables.py
+++ variables.py
@@ -1,20 +1,5 @@
-# These two variables (x and y) are both integers
-x = 4
-y = 5
+# Store the input of the user in the varialbe 'name'
+name = input("Enter your name: ")
 
-# The variable 'name' and 'father_name are strings
-name = "Abel"
-father_name = "Gideon"
-
-# This is a boolean variable
-is_student = True
-
-age = 19
-
-# Perform an addition between the two variables and display the output to the terminal
-# Output: 9
-print(x + y)
-
-# Convert the integarate 'age' into a string and then combine multiple strings into one
-# Output: Abel Gideon 19 years hold
-print(name + " " + father_name + " " + str(age) + " years old")
+# Greet the user
+print ("Hello " + name)
