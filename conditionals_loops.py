ff14d2baba05e7b1409fec585835e83424ad4bf2
KejPersonalStudy <jekmena.personal.study@gmail.com>
2025-02-24 09:45:13 +1100

    Create conditionals_loops.py

---
+++ conditionals_loops.py
@@ -0,0 +1,8 @@
+age = 19
+
+# Test the age if it is greater than 20, in this case it is not (or False)
+if age > 20:
+    print("You are older than 20")
+# The else condition is for any condition that is not met after testing the 'if' or 'elif' if there are multiple conditions.
+else:
+    print("You are younger than 20")
