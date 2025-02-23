4d78b7b735ebcb35f4ab4bb269b24713157b942f
KejPersonalStudy <jekmena.personal.study@gmail.com>
2025-02-23 21:35:33 +1100

    Update variables.py

--- variables.py
+++ variables.py
@@ -2,11 +2,18 @@
 x = 4
 y = 5
 
-# The variable 'name' is a string
+# The variable 'name' and 'father_name are strings
 name = "Abel"
+father_name = "Gideon"
+
 
 # This is a boolean variable
 is_student = True
 
 # Perform an addition between the two variables and display the output to the terminal
+# Output: 9
 print(x + y)
+
+# Combine the two string literal and add a space in between 
+# Output: Abel Gideon
+print(name + " " + father_name)
