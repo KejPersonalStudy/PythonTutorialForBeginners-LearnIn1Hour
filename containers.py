95fd5dd99806a73f38f41de80ce87a00579ee20f
KejPersonalStudy <jekmena.personal.study@gmail.com>
2025-02-24 15:06:57 +1100

    Lists in Python
    Simple Python script that demonstrates lists in Python

---
+++ containers.py
@@ -0,0 +1,10 @@
+# These three are lists in python and they support indexing, slicing, and other list operations
+name = ["Abel", "Abenezer"] # List containing two strings
+numbers = [1, 2, 3] # List containing three integers
+combo = ["Abel", 2, 3] # Combination of strings and integers
+
+# Display the lists in the terminal
+print(name)
+print(numbers)
+print(combo)
+print(name + combo)
