a4b3b1542a299f5bc908bd4aa3e187f95484d0be
KejPersonalStudy <jekmena.personal.study@gmail.com>
2025-02-24 14:55:05 +1100

    Using while loop for a simple guessing game

--- conditionals_loops.py
+++ conditionals_loops.py
@@ -1,5 +1,24 @@
-# The 'for' loop automatically initialize 'num' and assign it values from ranges 0 to 9
-# The loop then iteratates over these values on by one.
-# Print num for the current iteration
-for num in range(10):
-    print(num)
+# Guessing game
+
+# Store the number to be guessed by the user. Ideally, it should be random
+secret_number = 4
+# Initialize the guess variable to be used in the 'while' loop
+# Use '-1', so it is outside the guess range of (0 to 10)
+guess = -1;
+
+# Greet the user about the title and game mechanics
+print("Guessing Game")
+print("Hello, Mortal. I have a number in mind, can you guess it ranges from 0 to 10?")
+
+# This while loop has an infinite condition 'guess!= secret_number'. It will continue running until a break condition is met or externally stopped.
+# Ask the user to guess the 'secret_number' and prompt the user if its guess is high or low.
+while guess!= secret_number:
+    guess = int(input("Enter your guess: "))
+    if guess < secret_number:
+        print("Guess too low")
+    elif guess > secret_number:
+        print("Guess too high")
+    else:
+        print("Congrats, you have won")
+        
+        
