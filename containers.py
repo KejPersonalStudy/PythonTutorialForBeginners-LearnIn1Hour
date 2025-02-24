6b963e2bcf9e6402e808778a31bbd74db59579b1
KejPersonalStudy <jekmena.personal.study@gmail.com>
2025-02-24 16:11:52 +1100

    Update containers.py
    Various way of modifying a list in Python

--- containers.py
+++ containers.py
@@ -1,17 +1,31 @@
 # These three are lists in python and they support indexing, slicing, and other list operations
+from token import NUMBER
+
+
 name = ["Abel", "Abenezer"] # List containing two strings
 numbers = [1, 2, 3] # List containing three integers
 combo = ["Abel", 2, 3] # Combination of strings and integers
 
-# Display the lists in the terminal
-print(name)
+# Add an integer '4' to the 'numbers' list
+numbers.append(4)
 print(numbers)
-print(combo)
-print(name + combo)
 
-# Using For Loop. Print and iterate to each item in the combo list
-for item in combo:
-    print(item)
+# Insert the integer '100' on the index '1' (second in this case)
+numbers.insert(1,100)
+print(numbers)
 
-# Accessing a specific item on the 'numbers' list using index.
-print (numbers[1])
+# Add multiple elements instead of one to the end of the list.
+numbers.extend([300,400,25])
+print(numbers)
+
+# Reaarange the elements in the list in ascending order. 
+numbers.sort()
+print(numbers)
+
+# Reaarange the elements in the list in descending order. 
+numbers.reverse()
+print(numbers)
+
+# Delete the specific item on the list
+numbers.remove(400)
+print(numbers)
