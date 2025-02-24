f753eeed226adbbf735b0d961b346c12f4e28ebf
KejPersonalStudy <jekmena.personal.study@gmail.com>
2025-02-24 17:30:34 +1100

    Update files.py
    Reads numbers from a file, removes leading and trailing spaces, converts them to integers, sums them up, and display the total sum.

--- files.py
+++ files.py
@@ -7,9 +7,15 @@ file = open("Hello.txt", "r")
 # Read all the items in the text file per line and then display it
 numbers = file.readlines()
 
+sum = 0
+
 # Iterate to each line and remove leading and trailing characters from a string
+# Convert the string into integer and summate all the numbers in a single variable
 for number in numbers:
-    print(number.strip())
+    sum = sum + int(number.strip())
+
+# Display the total 
+print (sum)
 
 # Close the file
 file.close()
