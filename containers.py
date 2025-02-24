0c9bde59ed79b8eb3626310a8f2c260a305c95f3
KejPersonalStudy <jekmena.personal.study@gmail.com>
2025-02-24 16:25:50 +1100

    Update containers.py
    Simple example of dictionary

--- containers.py
+++ containers.py
@@ -1,31 +1,17 @@
 # These three are lists in python and they support indexing, slicing, and other list operations
-from token import NUMBER
-
-
 name = ["Abel", "Abenezer"] # List containing two strings
 numbers = [1, 2, 3] # List containing three integers
 combo = ["Abel", 2, 3] # Combination of strings and integers
 
-# Add an integer '4' to the 'numbers' list
-numbers.append(4)
-print(numbers)
+# This is an example of a dictionary, which is a collection of key-value pairs. 'One', 'Two'... are keys and '1', '2'... are values
+# Each key are unique and each key maps to a single value
+alphaNum = {
+    "One": 1,
+    "Two": 2,
+    "Three": 3
+}
 
-# Insert the integer '100' on the index '1' (second in this case)
-numbers.insert(1,100)
-print(numbers)
+# By defauly, using a 'for' loop on a dictionary will iterate or loop through the keys.
+for items in alphaNum:
+    print(items)
 
-# Add multiple elements instead of one to the end of the list.
-numbers.extend([300,400,25])
-print(numbers)
-
-# Reaarange the elements in the list in ascending order. 
-numbers.sort()
-print(numbers)
-
-# Reaarange the elements in the list in descending order. 
-numbers.reverse()
-print(numbers)
-
-# Delete the specific item on the list
-numbers.remove(400)
-print(numbers)
