60f2642c7164a39e22c61040231e5305c83b9ca1
KejPersonalStudy <jekmena.personal.study@gmail.com>
2025-02-24 18:13:06 +1100

    Implementing class with basic inheritance

--- classes.py
+++ classes.py
@@ -1,10 +1,32 @@
 # Create a class named 'Animal'
 class Animal:
+    # Implemented a constructor and assign the name to the instance
+    def __init__(self, name):
+        self.name = name
     # Self represents the instance calling this method
+
+    # Instance methods for the Animal Class...
     def eat(self):
         print("Eating...")
     def breathe(self):
         print("Breathing...")
 
-animal = Animal() # Create instance of the Animal Class
-animal.eat() # Call the eat method()
+# Define a 'Dog' Class that inherits from the Animal Class
+class Dog(Animal):
+    def sound(self):
+        print("Barking...")
+
+# Define a 'Cat' Class that inherits from the Animal Class
+class Cat(Animal):
+    def sound(self):
+        print("Meow...")
+
+# Create an instance of a dogm, store it in labradodle, and call the method from the Animal Class and the Dog Class
+labradodle = Dog("Labradodle")
+labradodle.eat()
+labradodle.sound()
+
+# Similar to the Above code.
+persian_cat = Cat("Persian Cat")
+persian_cat.sound()
+persian_cat.breathe()
